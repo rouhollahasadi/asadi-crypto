0631de8fd0363eb55a729f1b20f9568bf471c46d
rouhollahasadi <rouhollahasadi83@gmail.com>
2023-07-22 20:17:04 +0430

    Navabr changed

--- src/components/ui/Navbar.component.jsx
+++ src/components/ui/Navbar.component.jsx
@@ -1,11 +1,40 @@
 
 import React from 'react';
+import {Link} from 'react-router-dom';
+import { FaCoins } from 'react-icons/fa';
+import './Navbar.css';
+
 
 const NavbarComponent = () => {
     return (
-        <div>
-            <h4>NavbarComponent</h4>
-        </div>
+        <nav className="navbar navbar-expand-lg bg-body-tertiary">
+            <div className="container-fluid">
+                <div>
+                    <FaCoins className="navbar-logo-icon" />
+                    <span className="navbar-logo-text">Crypto</span>
+                </div>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarNav">
+                    <ul className="navbar-nav">
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/">Home</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/faq">faq</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/about-us">About us</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/contact-us">Contact us</Link>
+                        </li>
+               
+                    </ul>
+                </div>
+            </div>
+        </nav>
     );
 };
 
--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 
+// import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
