ec8bfe00c794675156774149a2c20695020bc460
rouhollahasadi <rouhollahasadi83@gmail.com>
2023-07-23 10:46:13 +0430

    test loader in home page

--- src/pages/Home.page.jsx
+++ src/pages/Home.page.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import SpinnerComponent from '../components/ui/Spinner.component';
 
 const HomePage = () => {
     return (
         <div>
             <h5>home page</h5>
             <h5>home page</h5>
+            <SpinnerComponent />
         </div>
     );
 };
