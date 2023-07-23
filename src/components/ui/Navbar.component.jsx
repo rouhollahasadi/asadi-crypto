3a1099f9974154bf11cbad1d9b0e0249c9c950d2
rouhollahasadi <rouhollahasadi83@gmail.com>
2023-07-23 10:03:51 +0430

    auto close menu button

--- src/components/ui/Navbar.component.jsx
+++ src/components/ui/Navbar.component.jsx
@@ -1,11 +1,15 @@
 
-import React from 'react';
+import React,{useRef} from 'react';
 import {Link} from 'react-router-dom';
 import { FaCoins } from 'react-icons/fa';
 import './Navbar.css';
 
 
 const NavbarComponent = () => {
+    const btnToggleRef = useRef();
+    const toggleMenu =() => {
+        btnToggleRef.current.click();
+    }
     return (
         <nav className="navbar navbar-expand-lg bg-body-tertiary">
             <div className="container-fluid">
@@ -13,21 +17,30 @@ const NavbarComponent = () => {
                     <FaCoins className="navbar-logo-icon" />
                     <span className="navbar-logo-text">Crypto</span>
                 </div>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <button 
+                 className="navbar-toggler" 
+                 type="button" 
+                 data-bs-toggle="collapse"
+                 data-bs-target="#navbarNav" 
+                 aria-controls="navbarNav" 
+                 aria-expanded="false" 
+                 aria-label="Toggle navigation"
+                 ref={btnToggleRef}
+                 >
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarNav">
                     <ul className="navbar-nav">
-                        <li className="nav-item">
+                        <li className="nav-item" onClick={toggleMenu}>
                             <Link className="nav-link" to="/">Home</Link>
                         </li>
-                        <li className="nav-item">
+                        <li className="nav-item" onClick={toggleMenu}>
                             <Link className="nav-link" to="/faq">faq</Link>
                         </li>
-                        <li className="nav-item">
+                        <li className="nav-item" onClick={toggleMenu}>
                             <Link className="nav-link" to="/about-us">About us</Link>
                         </li>
-                        <li className="nav-item">
+                        <li className="nav-item" onClick={toggleMenu}>
                             <Link className="nav-link" to="/contact-us">Contact us</Link>
                         </li>
                
