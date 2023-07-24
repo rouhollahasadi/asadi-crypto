6603d38dee971ebfe6f99b29c62faab6a30b3d41
rouhollahasadi <rouhollahasadi83@gmail.com>
2023-07-24 15:51:43 +0430

    coin details api

--- src/constans/api.js
+++ src/constans/api.js
@@ -1,1 +1,5 @@
 export const baseUrl = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false&locale=en';
+
+export const baseDetailUrl = (id) => {
+    return  `https://api.coingecko.com/api/v3/coins/${id}`;
+}
